fa3cb14de1c5ff92f2c9fb91760816472229acb9
M-Mazen <Mazenali.13@hotmail.com>
2022-04-19 17:35:40 +0300

    upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let clicked=document.querySelector(".mune");
+let linkss=document.querySelector(".linkss .linkss" );
+
+if(clicked.CssText="display:none"){
+    console.log("use")
+}
+clicked.addEventListener("click",(e)=>{
+
+        linkss.classList.toggle("clicked");
+    
+})
